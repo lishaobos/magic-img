e2f571dac199782e897992336fd53e6a47b24f14
lishaobos <long126@qq.com>
2022-12-08 09:16:25 +0800

    fix: ts declare

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import installMagicImg from 'magic-img'
-installMagicImg()
 import App from './App'
 import './index.css'
 import 'magic-img/css'
 
+installMagicImg()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
