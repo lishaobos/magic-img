5528832c906e13b559e74682045b8a27e9163eaa
lishaobos <long126@qq.com>
2022-12-11 15:26:07 +0800

    fix: 删除多余依赖 + nuxt 打包

--- src/vite/index.ts
+++ src/vite/index.ts
@@ -31,7 +31,7 @@ export default function (options: Options = {}): Plugin {
       const { magic, ...customParams } = Object.fromEntries(new URLSearchParams(params).entries())
       const { width = 0, height = 0, width_ = 0, height_ = 0, content } = await transformMap[magic](filePath, { ...options[magic], ...customParams })
       const [match] = Array.from(code.matchAll(pathReg));
-      const str = isDev ? `import src from '${match[2]}'` : `const src = '${match[2].slice(0, match[2].length - 2)}'`
+      const str = isDev ? `import src from "${match[2]}"` : `const src = "${match[2].slice(0, match[2].length - 2)}"`
       cacheMap.set(id, `
       ${str}
       export default JSON.stringify({
