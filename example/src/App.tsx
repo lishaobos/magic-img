ff1b067b978ba63e08d0969adab4758576c2bdef
lishaobos <www.long126@qq.com>
2023-03-10 21:42:10 +0800

    style: example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,63 +7,63 @@ import 'magic-img/css'
 
 
 import './App.css'
-import img1 from 'magic-img@https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989__480.jpg?time=123&magic=lqip'
-// import img1 from './assets/1.jpg?magic=lqip'
-import img2 from './assets/2.jpg?magic=lqip'
-import img3 from './assets/3.jpg?magic=lqip'
-import img4 from './assets/4.jpg?magic=lqip'
-import img5 from './assets/5.jpg?magic=blurhash'
-import img6 from './assets/6.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
-import img7 from './assets/7.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
-import img8 from './assets/8.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
-import img9 from './assets/9.jpg?magic=sqip&numberOfPrimitives=150'
-import img10 from './assets/10.jpg?magic=blurhash'
-import img11 from './assets/11.jpg?magic=draw'
-import img12 from './assets/12.jpg?magic=draw'
-import img13 from './assets/13.jpg?magic=draw'
-import img14 from './assets/14.jpg?magic=draw'
-import img15 from './assets/15.jpg?magic=blurhash'
+import img1 from 'magic-img@https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989__480.jpg?time=123&magic=blurhash'
+// import img1 from './assets/1.jpg?magic=blurhash'
+import img2 from './assets/2.jpg?magic=blurhash'
+import img3 from './assets/3.jpg?magic=blurhash'
+import img4 from './assets/4.jpg?magic=blurhash'
+import img5 from './assets/5.jpg?magic=lqip'
+import img6 from './assets/6.jpg?magic=lqip'
+import img7 from './assets/7.jpg?magic=lqip'
+import img8 from './assets/8.jpg?magic=lqip'
+import img9 from './assets/9.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
+import img10 from './assets/10.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
+import img11 from './assets/11.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
+import img12 from './assets/12.jpg?magic=sqip&numberOfPrimitives=100&blur=0'
+import img13 from './assets/13.jpg?magic=cucoloris'
+import img14 from './assets/14.jpg?magic=cucoloris'
+import img15 from './assets/15.jpg?magic=cucoloris'
 import img16 from './assets/16.jpg?magic=cucoloris'
-import img17 from './assets/17.jpg?magic=cucoloris'
-import img18 from './assets/18.jpg?magic=cucoloris'
-import img19 from './assets/19.jpg?magic=cucoloris'
-import img20 from './assets/20.jpg?magic=blurhash'
+import img17 from './assets/17.jpg?magic=draw'
+import img18 from './assets/18.jpg?magic=draw'
+import img19 from './assets/19.jpg?magic=draw'
+import img20 from './assets/20.jpg?magic=draw'
 
 function App() {
 
 	const blurhash: string[] = [
-		img5,
-		img10,
-		img15,
-		img20
-	]
-
-	const lqip: string[] = [
 		img1,
 		img2,
 		img3,
-		img4,
+		img4
 	]
 
-	const sqip: string[] = [
+	const lqip: string[] = [
+		img5,
 		img6,
 		img7,
 		img8,
-		img9,
 	]
 
-	const draw: string[] = [
+	const sqip: string[] = [
+		img9,
+		img10,
 		img11,
 		img12,
-		img13,
-		img14,
 	]
 
 	const cucoloris: string[] = [
+		img13,
+		img14,
+		img15,
 		img16,
+	]
+
+	const draw: string[] = [
 		img17,
 		img18,
 		img19,
+		img20,
 	]
 
 	const imgMap = new Map<string, string[]>([
